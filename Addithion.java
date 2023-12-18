48bcdef533083f25987d2b902130f66bf5ce441c
0rijwan123 <mdrijwan4jee@gmail.com>
2023-12-18 16:59:28 +0530

    second commit

--- Addithion.java
+++ Addithion.java
@@ -1,6 +1,8 @@
 import java.util.*;
 public class Addition{
     public static void main(String [] args){
-        System.out.println("Hello world:");
+        int a = 1;
+        int b=2;
+        System.out.println(a+b);
     }
 }
